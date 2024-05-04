323a61fcd7a8dfbdc3730057ac0ed4a450f21ea6
Andrey Lavor <lavor.andrey@gmail.com>
2024-05-04 12:56:07 +0600

    Renaming

---
+++ intellispacesframework-dynamicproxy-test/src/main/java/tech/intellispacesframework/dynamicproxy/test/samples/AbstractClass.java
@@ -0,0 +1,8 @@
+package tech.intellispacesframework.dynamicproxy.test.samples;
+
+public abstract class AbstractClass {
+
+  public abstract int method1(int argument);
+
+  public abstract String method2(String argument);
+}
---
+++ intellispacesframework-dynamicproxy-test/src/main/java/tech/intellispacesframework/dynamicproxy/test/samples/TrackedInterface.java
@@ -0,0 +1,8 @@
+package tech.intellispacesframework.dynamicproxy.test.samples;
+
+public interface TrackedInterface {
+
+  String method1();
+
+  String method2(int i);
+}
